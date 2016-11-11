9d1fb86c77243f223a5483cc67188a939b0ad59a
Andrey Dolgachev <andrey.dolgachev@futureadvisor.com>
2016-11-11 11:09:55 -0800

    Add a couple more variants of usage

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import 'react-toolbox/lib/commons.scss';           // Import common styles
 import PurpleAppBar from './PurpleAppBar.js';      // AppBar with simple overrides
 import SuccessButton from './SuccessButton.js';    // A button with complex overrides
+import CancelButton from './CancelButton.js';         // A button imported as a raw component
 import { Button } from 'react-toolbox/lib/button'; // Bundled component import
 
 const App = () => (
@@ -10,6 +11,8 @@ const App = () => (
     <section style={{ padding: 20 }}>
       <SuccessButton label='Success' primary raised />
       <Button label='Primary Button' primary />
+      <CancelButton label='Cancel' primary raised />
+      <Button label='Styled Button' primary style={{ color: 'red' }} />
     </section>
   </div>
 );
---
+++ src/app/CancelButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from 'react-toolbox/lib/button/Button';
+import theme from './CancelButton.scss';
+
+const CancelButton = (props) => <Button {...props} theme={theme} />;
+export default CancelButton;
